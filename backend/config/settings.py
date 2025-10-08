6d7a8726cd0f6edebd29deed9cb041fd2838faee
Dawid <dawid_pludowski@wp.pl>
2025-10-08 17:16:11 +0200

    feat(backend): add X-Request-ID middleware for request tracing

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "config.middleware.request_id.RequestIDMiddleware",
 ]
 
 # CORS
