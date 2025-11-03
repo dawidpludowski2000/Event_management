76a86608aa33100646a0b949358629bc302255e1
Dawid <dawid_pludowski@wp.pl>
2025-11-03 12:37:55 +0100

    feat(backend): activate Sentry monitoring in production

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -63,3 +63,11 @@ urlpatterns = [
     # --- oficjalne v1 ---
     path("api/v1/", include((api_v1_patterns, "apiv1"))),
 ]
+
+# --- SENTRY TEST ENDPOINT ---
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+urlpatterns += [
+    path("sentry-debug/", trigger_error),
+]
